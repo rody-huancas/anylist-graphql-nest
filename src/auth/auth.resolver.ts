df73b41a0cb7267aba159bae6dd46b13560b53aa
Rody Huancas <rodyhuancas.04@gmail.com>
2024-05-15 19:29:11 -0500

    Add: auth guard

--- src/auth/auth.resolver.ts
+++ src/auth/auth.resolver.ts
@@ -1,7 +1,9 @@
-import { Args, Mutation, Query, Resolver } from '@nestjs/graphql';
+import { UseGuards } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { AuthResponse } from './types/auth-response.type';
+import { JwtAuthGuard } from './guards/jwt-auth.guard';
 import { LoginInput, SignupInput } from './dto/inputs';
+import { Args, Mutation, Query, Resolver } from '@nestjs/graphql';
 
 @Resolver()
 export class AuthResolver {
@@ -21,8 +23,13 @@ export class AuthResolver {
     return this.authService.login( loginInput );
   }
 
-  // @Query({}, { name: 'revalidate' })
-  // async revalidateToken(): Promise<> {
-  //   return this.authService.revalidate();
-  // }
+  @Query( () => AuthResponse, { name: 'revalidate' } )
+  @UseGuards( JwtAuthGuard )
+  revalidateToken(
+    // @CurrentUser user: User
+  ): AuthResponse {
+    
+    // return this.authService.revalidateToken()
+    throw new Error("Not implemented");
+  }
 }
