daad77cbf50101793f24a992aeec042090d8fce6
Rody Huancas <rodyhuancas.04@gmail.com>
2024-05-27 20:30:53 -0500

    Add: access by role in findOne users

--- src/users/users.resolver.ts
+++ src/users/users.resolver.ts
@@ -2,7 +2,7 @@ import { Resolver, Query, Mutation, Args, ID } from '@nestjs/graphql';
 import { UsersService } from './users.service';
 import { User } from './entities/user.entity';
 import { ValidRolesArgs } from './dto/args/roles.arg';
-import { UseGuards } from '@nestjs/common';
+import { ParseUUIDPipe, UseGuards } from '@nestjs/common';
 import { JwtAuthGuard } from 'src/auth/guards/jwt-auth.guard';
 import { CurrentUser } from 'src/auth/decorators/current-user.decorator';
 import { ValidRoles } from 'src/auth/enums/valid-roles.enum';
@@ -15,14 +15,17 @@ export class UsersResolver {
   @Query(() => [User], { name: 'users' })
   findAll(
     @Args() validRoles: ValidRolesArgs,
-    @CurrentUser([ValidRoles.admin]) user: User
+    @CurrentUser([ValidRoles.admin, ValidRoles.superUser]) user: User
   ): Promise<User[]> {
     return this.usersService.findAll( validRoles.roles );
   }
 
   @Query(() => User, { name: 'user' })
-  findOne(@Args('id', { type: () => ID }) id: string): Promise<User> {
-    throw new Error('Not implemented');
+  findOne(
+    @Args('id', { type: () => ID }, ParseUUIDPipe) id: string,
+    @CurrentUser([ValidRoles.admin, ValidRoles.superUser]) user: User
+  ): Promise<User> {
+    return this.usersService.findOneById(id);
   }
 
   @Mutation(() => User)
