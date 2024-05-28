31fa65161145ed16da0b5785aedd255ac5f9e4ff
Rody Huancas <rodyhuancas.04@gmail.com>
2024-05-27 19:51:41 -0500

    Add: validation by role in user resolver

--- src/users/users.resolver.ts
+++ src/users/users.resolver.ts
@@ -2,14 +2,20 @@ import { Resolver, Query, Mutation, Args, ID } from '@nestjs/graphql';
 import { UsersService } from './users.service';
 import { User } from './entities/user.entity';
 import { ValidRolesArgs } from './dto/args/roles.arg';
+import { UseGuards } from '@nestjs/common';
+import { JwtAuthGuard } from 'src/auth/guards/jwt-auth.guard';
+import { CurrentUser } from 'src/auth/decorators/current-user.decorator';
+import { ValidRoles } from 'src/auth/enums/valid-roles.enum';
 
 @Resolver(() => User)
+@UseGuards(JwtAuthGuard)
 export class UsersResolver {
   constructor(private readonly usersService: UsersService) {}
 
   @Query(() => [User], { name: 'users' })
   findAll(
-    @Args() validRoles: ValidRolesArgs
+    @Args() validRoles: ValidRolesArgs,
+    @CurrentUser([ValidRoles.admin]) user: User
   ): Promise<User[]> {
     return this.usersService.findAll( validRoles.roles );
   }
